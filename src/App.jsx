29b9408ccbb4c61f2af2c5fc81d69ec6ec4f6efa
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-31 07:08:49 +0300

    i connec signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from './auth/signup'
+
 function App() {
 
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App
