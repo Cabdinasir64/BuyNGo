a88e486d6f8cc42c82fbd915614f657bec3afe63
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-31 06:30:46 +0300

    i make custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#4FD1C5",
+          DEFAULT: "#38B2AC",
+          dark: "#319795",
+        },
+        secondary: {
+          light: "#F6AD55",
+          DEFAULT: "#ED8936",
+          dark: "#DD6B20",
+        },
+        accent: {
+          red: "#E53E3E",
+          green: "#48BB78",
+        },
+        dark: {
+          DEFAULT: "#2D3748",
+          muted: "#718096",
+        },
+      },
+      fontFamily: {
+        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        heading: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
