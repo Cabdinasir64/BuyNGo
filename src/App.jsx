8a48acacecd95e172b6203dbc7521a0988ab6b0f
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-31 12:57:58 +0300

    i connect another files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from './pages/admin/admin'
+import SignUp from './auth/signup'
+import SignIn from './auth/signin'
 function App() {
 
 
@@ -8,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<AdminDashboard />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </Router>
     </>
