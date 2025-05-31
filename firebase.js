8cef2f31889270b621d8f6384df71a8645c67995
Cabdinasir64 <cabdinasira64@gmail.com>
2025-05-31 09:15:24 +0300

    i change initialize

--- firebase.js
+++ firebase.js
@@ -1,19 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
+// Config
 const firebaseConfig = {
   apiKey: "AIzaSyAtwinHNzdzegzSObfcjbo85Gz6gyMUDts",
   authDomain: "buyngo-a5545.firebaseapp.com",
   projectId: "buyngo-a5545",
-  storageBucket: "buyngo-a5545.firebasestorage.app",
+  storageBucket: "buyngo-a5545.appspot.com",
   messagingSenderId: "1040711699061",
   appId: "1:1040711699061:web:9a6c6ca2fdd2420009863c",
   measurementId: "G-5FL0MPKZ7G",
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
-export const facebookProvider = new FacebookAuthProvider();
+// Initialize
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+
+export default firebase;
