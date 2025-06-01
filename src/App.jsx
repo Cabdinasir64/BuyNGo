63f9fc0732c0f55f9b68cd8dcd7c08e5c0271323
Cabdinasir64 <cabdinasira64@gmail.com>
2025-06-01 20:37:59 +0300

    i connect another pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import SignUp from './auth/signup'
 import SignIn from './auth/signin'
 import SellerDashboard from './pages/seller/seller'
 import AdminDashboard from "./pages/admin/admin";
+import Navbar from './components/navbar'
+import Category from './pages/category'
+
 
 function App() {
 
@@ -11,7 +14,10 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<SellerDashboard />} />
+          <Route path="/" element={<Navbar />} />
+          <Route path="/category/:category" element={<Category />} />
+          <Route path="/category/:category/:subcategory" element={<Category />} />
+          <Route path="/SellerDashboard" element={<SellerDashboard />} />
           <Route path="/AdminDashboard" element={<AdminDashboard />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
