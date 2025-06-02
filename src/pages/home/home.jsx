041855c05a3bd19058f7b31dc68e4c4ee6c18100
Cabdinasir64 <cabdinasira64@gmail.com>
2025-06-02 06:49:12 +0300

    i make new file home

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import react from 'react-dom'
+import Navbar from '../../components/navbar'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+export default Home;
