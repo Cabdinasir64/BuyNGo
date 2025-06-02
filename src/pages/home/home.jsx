31944fe28a1f28cb9e15f692f77a450f66475681
Cabdinasir64 <cabdinasira64@gmail.com>
2025-06-02 15:17:58 +0300

    i connect another pages

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,21 @@
 import react from 'react-dom'
 import Navbar from '../../components/navbar'
 import Hero from './components/hero'
+import Product from "./components/product";
+import Features from './components/features';
+import Testimonials from './components/testimonial';
+import Footer from "./components/footer";
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
+            <Product />
+            <Features />
+            <Testimonials />
+            <Footer />
         </>
-    )
+    );
 }
 export default Home;
