e64211d31d7ca08558ee7bcc400a634f4d0bab9e
Cabdinasir64 <cabdinasira64@gmail.com>
2025-06-02 11:34:51 +0300

    i change home location

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import react from 'react-dom'
 import Navbar from '../../components/navbar'
+import Hero from './components/hero'
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <Hero />
         </>
     )
 }
