f4cd2e92d5994a02a24930563de835bce5f57f20
Cabdinasir64 <cabdinasira64@gmail.com>
2025-06-07 12:13:09 +0300

    i make the test

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,23 +1,32 @@
-import react from 'react-dom'
-import Navbar from '../../components/navbar'
-import Hero from './components/hero'
+import react from "react-dom";
+import Navbar from "../../components/navbar";
+import Hero from "./components/hero";
 import Product from "./components/product";
-import Features from './components/features';
-import Testimonials from './components/testimonial';
+import Features from "./components/features";
+import Testimonials from "./components/testimonial";
 import Faq from "./components/faq";
 import Footer from "../../components/footer";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const Home = () => {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <Product />
-            <Features />
-            <Testimonials />
-            <Faq />
-            <Footer />
-        </>
-    );
-}
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // scroll to top
+  }, [pathname]);
+
+  return null;
+};
+return (
+  <>
+    <Navbar />
+    <Hero />
+    <Product />
+    <Features />
+    <Testimonials />
+    <Faq />
+    <Footer />
+  </>
+);
 export default Home;
